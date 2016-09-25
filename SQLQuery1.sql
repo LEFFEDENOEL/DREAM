b1bc074fc8a95739caf8cffad2e506b223038bfd
Laurent FAURE <laurent.faure@2isa.org>
2016-09-25 16:35:02 +0200

    DEPOT INITIAL WINFORM

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,75 +1,43 @@
-﻿create procedure DECHIFFREMENT 
+﻿if exists (select 1
+            from  sys.procedures
+            where  name ='FABRIQUELOGIN'
+           )
+drop procedure dbo.FABRIQUELOGIN
+go
 
-@mdpfromclient varchar(32), 
-@login varchar(32), 
-@resultat bit output
+
+create procedure FABRIQUELOGIN
+
+@nom varchar(28),
+@prenom varchar(25),
+@logincalcule varchar(28) out
 
 as
 
-begin 
---10
-	declare
-		
-	@mdpclair as varchar(32),
-	@mdpcrypte as varbinary(256)
-	
-	/* Ouverture de la clé de chiffrement */
-    begin try
-	
-        open SYMMETRIC key CLE_PASS
-        DECRYPTION by PASSWORD = 'L..jstp !_x';
---21		
-    end try
-    begin catch
-        -- Handle non-existant key here
-    end catch
-	
-	/* Déchiffrement du mot de passe dans la colonne MDP_UTILISATEUR */
-	set @mdpcrypte = (select MDP_UTILISATEUR from UTILISATEURS where LOGIN_UTILISATEUR = @login)	
-	set @mdpclair = (select cast(DECRYPTBYKEY(MDP_UTILISATEUR) as varchar(32))from UTILISATEURS where LOGIN_UTILISATEUR = @login)
---30		
-	/* Test de correspondance des mots de passe */
-	if @mdpclair != @mdpfromclient 
-	set @resultat = 0
-	else 
-	set @resultat = 1;
-		
-	return @resultat;
---38	
-	/* Fermeture de la clé de chiffrement */
-	close SYMMETRIC key CLE_PASS;
+declare 
+
+@compteur as int = 0,
+@nombre_aleatoire as varchar(4)
+
+begin
+
+set @nombre_aleatoire =  substring ( cast(floor ((9000*rand())+1000)as varchar(50)) , 1 , 4 );
+
+	while @compteur < 5
+
+			begin		
+			set @nom = @nom + substring(@prenom, @compteur, 1);
+			set @compteur = @compteur + 1;
+			end
 			
+	set @logincalcule = @nom + @nombre_aleatoire	
+
 end
-
-
-declare
-@resultat as bit
-execute dbo.dechiffrement 'fau', 'FAURELAU', @resultat output
-print @resultat
-
-if exists (select 1
-            from  sys.procedures
-            where  name ='DECHIFFREMENT'
-           )
-drop procedure dbo.DECHIFFREMENT
 go
 
 
-insert into UTILISATEURS (NOM_UTILISATEUR, PRENOM_UTILISATEUR, EST_MODERATEUR, MAIL_UTILISATEUR, LOGIN_UTILISATEUR, MDP_UTILISATEUR, PSEUDO_UTILISATEUR, DATE_INSCRIPTION)
-values ('DUSSEUX','Loïc','TRUE','loic.dusseux@2isa,org','DUSSEUXLOI', CONVERT(varbinary(max), HASHBYTES('SHA1','duss')),'vivelaleffe','20160703 15:15:31');
+declare @logincalcule varchar(28)
+execute dbo.FABRIQUELOGIN 'FAURE', 'LAURENT', @logincalcule out
+print @logincalcule
 go
 
-
-insert into UTILISATEURS (NOM_UTILISATEUR, PRENOM_UTILISATEUR, EST_MODERATEUR, MAIL_UTILISATEUR, LOGIN_UTILISATEUR, MDP_UTILISATEUR, PSEUDO_UTILISATEUR, DATE_INSCRIPTION)
-values ('KECHAI','Valérie','FALSE','vk@2isa.com','KECHAIVAL', CONVERT(varbinary(max), HASHBYTES('SHA1','kech')),'ibiforever','20160703 15:15:37');
-go
-
-
-insert into UTILISATEURS (NOM_UTILISATEUR, PRENOM_UTILISATEUR, EST_MODERATEUR, MAIL_UTILISATEUR, LOGIN_UTILISATEUR, MDP_UTILISATEUR, PSEUDO_UTILISATEUR, DATE_INSCRIPTION)
-values ('MAZAUD','Catherine','FALSE','cmz@2isa.com','MAZAUDCAT', CONVERT(varbinary(max), HASHBYTES('SHA1','maza')),'duchesse','20160703 15:15:38');
-go
-
-
-insert into UTILISATEURS (NOM_UTILISATEUR, PRENOM_UTILISATEUR, EST_MODERATEUR, MAIL_UTILISATEUR, LOGIN_UTILISATEUR, MDP_UTILISATEUR, PSEUDO_UTILISATEUR, DATE_INSCRIPTION)
-values ('COIFFARD','Régis','FALSE','rc@2isa.com','COIFFARD', CONVERT(varbinary(max), HASHBYTES('SHA1','coif')),'credo2isa','20160703 15:15:39');
-go
